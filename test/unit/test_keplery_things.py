09e16f1697ca4d4cc7369190561a1b8992df4f25
endlessloop2 <mateo@endlessloop.me>
2019-02-22 00:34:32 -0300

    Updated tests

--- test/unit/test_keplery_things.py
+++ test/unit/test_keplery_things.py
@@ -7,12 +7,12 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../
 
 @pytest.fixture
 def valid_kepler_address(network='mainnet'):
-    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui' if (network == 'testnet') else 'XpjStRH8SgA6PjgebtPZqCa9y7hLXP767n'
+    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui' if (network == 'testnet') else 'KEdD53mxQmBz78kCRT6NdGiZvZH91D64AT'
 
 
 @pytest.fixture
 def invalid_kepler_address(network='mainnet'):
-    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Uj' if (network == 'testnet') else 'XpjStRH8SgA6PjgebtPZqCa9y7hLXP767m'
+    return 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Uj' if (network == 'testnet') else 'KEdD53mxQmBz78kCRT6NdGiZvZH91D64AT'
 
 
 @pytest.fixture
